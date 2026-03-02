715ba6976321942be9094ce6c14c50d3dd3e1204
vunc.vtu <vunc.vtu@hcmict.io>
2026-03-02 10:03:34 +0700

    [fix] adjust upload Cloudinary config

--- casangelina-strapi/config/plugins.js
+++ casangelina-strapi/config/plugins.js
@@ -12,30 +12,7 @@ module.exports = ({ env }) => ({
         uploadStream: {},
         delete: {},
       },
-      breakpoints: {
-        large: 0,
-        medium: 0,
-        small: 0,
-      },
       sizeLimit: 10 * 1024 * 1024, // 10MB
-      security: {
-        allowedFileTypes: [
-          'image/jpeg',
-          'image/png',
-          'image/gif',
-          'image/webp',
-          'image/svg+xml',
-          'image/avif',
-          'video/mp4',
-          'video/webm',
-          'application/pdf',
-        ],
-        allowedFileExtensions: [
-          '.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg', '.avif',
-          '.mp4', '.webm',
-          '.pdf',
-        ],
-      },
     },
   },
 });
