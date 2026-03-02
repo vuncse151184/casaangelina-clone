9dfdca89db3e188a9b71c9bc7b0fd2c819943608
vunc.vtu <vunc.vtu@hcmict.io>
2026-03-02 11:00:31 +0700

    [chore] adjust cloudinary config

--- casangelina-strapi/config/plugins.js
+++ casangelina-strapi/config/plugins.js
@@ -1,4 +1,8 @@
 module.exports = ({ env }) => ({
+  // Disable Strapi Cloud's built-in upload provider so Cloudinary is used
+  cloud: {
+    enabled: false,
+  },
   upload: {
     config: {
       provider: '@strapi/provider-upload-cloudinary',
